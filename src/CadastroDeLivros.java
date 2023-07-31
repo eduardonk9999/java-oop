51864295569a16786cf236baa901a927357a7708
Eduardo Silva <thiago.novak7@gmail.com>
2023-07-31 11:09:46 -0300

    Livro

---
+++ src/CadastroDeLivros.java
@@ -0,0 +1,8 @@
+public class CadastroDeLivros {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+        livro.nome = "Java 8 Prático";
+
+        System.out.println(livro.nome);
+    }
+}
